8bc4d6de914210c6d502412a35a592f6cb73c8fb
ironsoul0 <timka2609@gmail.com>
2020-08-01 14:48:02 +0600

    some more changes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,7 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.root}>
-      <h1>Hi, I am your next great app, what?</h1>
+      <h1>Hi, am I your next great app?</h1>
     </div>
   );
 }
