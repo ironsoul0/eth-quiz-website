09f2f118d0444f8dd0ae618de6a9b2fcedaa5682
ironsoul0 <timka2609@gmail.com>
2020-08-01 17:29:50 +0600

    temp commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,16 @@ import React from "react";
 import styles from "./App.module.css";
 import Topic from "../Topic";
 
-
 function App() {
   return (
     <div className={styles.root}>
       <h1>Hi, am I your next great app?</h1>
-      <Topic topic="Privacy" url="https://source.unsplash.com/user/erondu/600x400" done="8" total="10"/>
+      <Topic
+        topic="Privacy"
+        url="https://source.unsplash.com/user/erondu/600x400"
+        done="8"
+        total="10"
+      />
     </div>
   );
 }
--- src/components/Topic/index.js
+++ src/components/Topic/index.js
@@ -3,19 +3,21 @@ import styles from "./Topic.module.css";
 import clsx from "clsx";
 
 function Topic(props) {
-  var style = { 
-    backgroundImage: 'url(' + props.url + ')',
-    width:'280px',
-    height:'173px',
-    borderRadius: '10px',
-};
+  var style = {
+    backgroundImage: "url(" + props.url + ")",
+    width: "280px",
+    height: "173px",
+    borderTopLeftRadius: "10px",
+    borderTopRightRadius: "10px",
+  };
 
   return (
     <div className={styles.card}>
-        <div style={style}></div>
-        <h3 className={styles.name}>{props.topic}</h3>
-        <p className={styles.stat}>{props.done}/{props.total}</p>
-      
+      <div style={style}></div>
+      <h3 className={styles.name}>{props.topic}</h3>
+      <p className={styles.stat}>
+        {props.done}/{props.total}
+      </p>
     </div>
   );
 }
