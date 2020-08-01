d69a934d0a24ac1803019808b1c1e4a6adcd59ff
ironsoul0 <timka2609@gmail.com>
2020-08-01 16:08:06 +0600

    stabilize nav

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,26 @@
 import React from "react";
 
+import { Switch, Route, Link, Redirect } from "react-router-dom";
+import Navbar from "../Navbar";
 import styles from "./App.module.css";
 import Topic from "../Topic";
 
 function App() {
   return (
     <div className={styles.root}>
-      <h1>Hi, am I your next great app?</h1>
-      <Topic
-        topic="Privacy"
-        url="https://source.unsplash.com/user/erondu/600x400"
-        done="8"
-        total="10"
-      />
+      <Navbar />
+      <Switch>
+        <Route path="/challenges">
+          <h1>How about challenges?</h1>
+        </Route>
+        <Route path="/leaderboard">
+          <h1>How about leaderboard?</h1>
+        </Route>
+        <Route path="/knowledge">
+          <h1>Knowledge base?</h1>
+        </Route>
+        <Redirect to="/challenges" />
+      </Switch>
     </div>
   );
 }
