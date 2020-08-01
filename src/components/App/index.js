59f6c69b9da3ec28a884e243548ea9b7d5e2eb8c
ironsoul0 <timka2609@gmail.com>
2020-08-01 14:42:13 +0600

    add minimal styles

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.root}>
+      <h1>Hi, I am your next great app, what?</h1>
+    </div>
+  );
+}
+
+export default App;
