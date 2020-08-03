4bf8ed75daab692e650a2bc687dc5cb1e2324182
ironsoul0 <timka2609@gmail.com>
2020-08-03 16:19:05 +0600

    improve challenges

--- src/pages/challenges/index.js
+++ src/pages/challenges/index.js
@@ -10,16 +10,56 @@ function Challenges() {
     <div className={styles.root}>
       <Header>Topics</Header>
       <div className={styles.topics}>
-        <Topic done="5" total="10" />
-        <Topic done="5" total="10" />
-        <Topic done="5" total="10" />
-        <Topic done="5" total="10" />
+        <Topic
+          url="https://source.unsplash.com/user/erondu/600x400"
+          done="5"
+          total="10"
+          topic="ZK"
+        />
+        <Topic
+          url="https://source.unsplash.com/user/erondu/600x400"
+          done="5"
+          total="10"
+          topic="Security"
+        />
+        <Topic
+          url="https://source.unsplash.com/user/erondu/600x400"
+          done="5"
+          total="10"
+          topic="Proofs"
+        />
+        <Topic
+          url="https://source.unsplash.com/user/erondu/600x400"
+          done="5"
+          total="10"
+          topic="DApp Dev"
+        />
       </div>
       <div className={styles.topics}>
-        <Topic done="5" total="10" />
-        <Topic done="5" total="10" />
-        <Topic done="5" total="10" />
-        <Topic done="5" total="10" />
+        <Topic
+          url="https://source.unsplash.com/user/erondu/600x400"
+          done="5"
+          total="10"
+          topic="ZK"
+        />
+        <Topic
+          url="https://source.unsplash.com/user/erondu/600x400"
+          done="5"
+          total="10"
+          topic="Security"
+        />
+        <Topic
+          url="https://source.unsplash.com/user/erondu/600x400"
+          done="5"
+          total="10"
+          topic="Proofs"
+        />
+        <Topic
+          url="https://source.unsplash.com/user/erondu/600x400"
+          done="5"
+          total="10"
+          topic="DApp Dev"
+        />
       </div>
     </div>
   );
