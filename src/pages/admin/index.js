cc698e4d10e071b5e7627061c1395e99496afdda
ironsoul0 <timka2609@gmail.com>
2020-08-12 10:36:33 +0600

    add protection for admin page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,6 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 function Admin() {
+  const [authorized, setAuthorized] = useState(true);
+
+  React.useEffect(() => {
+    axios
+      .get("/quiz/is-supervisor")
+      .then((response) => {
+        setAuthorized(response.data.is_supervisor);
+      })
+      .catch(() => setAuthorized(false));
+  }, []);
+
+  if (!authorized) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <div>
       <p>Body</p>
