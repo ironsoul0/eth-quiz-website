d4b93b557e9b476f465481c8f3a6362ca1537af2
ironsoul0 <timka2609@gmail.com>
2020-08-12 13:30:46 +0600

    add deletion of topics

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
+import styles from "./Admin.module.css";
 
 function Admin() {
   const [authorized, setAuthorized] = useState(true);
+  const [topics, setTopics] = useState(null);
 
   React.useEffect(() => {
     axios
@@ -12,15 +14,55 @@ function Admin() {
         setAuthorized(response.data.is_supervisor);
       })
       .catch(() => setAuthorized(false));
+
+    axios.get("/quiz/get-user-scores").then(({ data }) => {
+      setTopics(data);
+    });
   }, []);
+
+  const handleDeleteTopic = (id) => {
+    const ask = window.confirm("Are you sure?");
+
+    if (!ask) {
+      return;
+    }
+
+    axios
+      .post("/quiz/delete-topic", {
+        id,
+      })
+      .then(() => {
+        setTopics(topics.filter((topic) => topic.topic_id !== id));
+      })
+      .catch(() => {
+        console.log("Error deleting");
+      });
+  };
 
   if (!authorized) {
     return <Redirect to="/" />;
   }
 
+  if (!topics) {
+    return <p>Loading..</p>;
+  }
+
   return (
     <div>
-      <p>Body</p>
+      <h2 style={{ marginBottom: "30px" }}>All topics</h2>
+      {topics.map((topic) => {
+        return (
+          <div key={topic.topic_id} className={styles.card}>
+            <h4>{topic.topic_name}</h4>
+            <button
+              onClick={() => handleDeleteTopic(topic.topic_id)}
+              className={styles.deleteButton}
+            >
+              Delete topic
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
